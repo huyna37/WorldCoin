97cef9273640e0fef2d5f9eec48c912bdc921e87
huy <adultsforfuture@gmail.com>
2024-10-20 23:11:57 +0700

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 
 export default async function Home() {
-    const session = await getSession();
     return (
         <div>
             <main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export default async function Home() {
-  const data = await fetch('http://localhost:3000/api/home')
-  let posts = await data.json()
   return (
     <div>
       <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
+    
     </ul>
     </div>
   );
